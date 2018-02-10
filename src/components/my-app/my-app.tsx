9624b4b02ebe9ba8fe0aecf9fe1dbe5562bdbaf2
Jedidiah Weller <jedi@webjunto.com>
2018-02-10 16:51:44 -0500

    Adding navigation bar and starting the body layout

--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -11,8 +11,44 @@ export class MyApp {
     return (
       <div>
         <header>
-          <h1>Stencil App Starter</h1>
+          <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow"> 
+            <h5 class="my-0 mr-md-auto font-weight-normal"> @jedihacks </h5>
+            <nav class="my-2 my-md-0 mr-md-3">
+              <a class="p-2 text-dark" href="3">Features </a>
+              <a class="p-2 text-dark" href="3">Features </a>
+              <a class="p-2 text-dark" href="3">Features </a>
+              <a class="p-2 text-dark" href="3">Features </a>
+            </nav>
+            <a class="btn btn-outline-primary" href="#"> Sign Up</a>
+          </div>
+          
         </header>
+
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light main-background">
+          <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+            <div class="card-deck mb-3 text-center">
+              <div class="card mb-4 box-shadow"> 
+                <div class="card-header">2014 </div>
+                <div class="card-body">
+                  <h5 class="card-title" >Founded Webjunto.com, hosted even in Dublin Ireland, and others</h5>
+                </div>
+              </div>
+              <div class="card mb-4 box-shadow"> 
+                <div class="card-header">2014 </div>
+                <div class="card-body">
+                  <h5 class="card-title" >Founded Webjunto.com, hosted even in Dublin Ireland, and others</h5>
+                </div>
+              </div>
+              <div class="card mb-4 box-shadow"> 
+                <div class="card-header">2014 </div>
+                <div class="card-body">
+                  <h5 class="card-title" >Founded Webjunto.com, hosted even in Dublin Ireland, and others</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
 
         <main>
           <stencil-router>
